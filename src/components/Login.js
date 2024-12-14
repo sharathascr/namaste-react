526cc4727495518036e68a78aad151479847e289
sharath160k@gmail.com <sharath160k@gmail.com>
2024-12-14 12:00:40 +0530

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,21 @@ import React from "react";
 import "../styles/Login.css";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login-page">
+      <div className="login-content">
+        <p className="login-heading"> Login</p>
+        <p className="create-account-text">
+          or <span className="create-text">create an account</span>
+          <div>
+            <input placeholder="Phone Number" />
+            <input placeholder="password" />
+            <button className="login-btn">Log</button>
+          </div>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
