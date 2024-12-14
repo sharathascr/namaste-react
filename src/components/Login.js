e6cf7c9fdee28a61d44d93385823bc440347da05
sharath160k@gmail.com <sharath160k@gmail.com>
2024-12-14 17:09:30 +0530

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,47 @@
 import React from "react";
 import "../styles/Login.css";
+import { useForm } from "react-hook-form";
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const handleLogin = () => {
+    console.log("Login Submitted");
+  };
   return (
     <div className="login-page">
       <div className="login-content">
         <p className="login-heading"> Login</p>
         <p className="create-account-text">
           or <span className="create-text">create an account</span>
-          <div className="login-inputs">
-            <input placeholder="Phone Number" />
-            <input placeholder="password" />
-            <button className="login-btn">Login</button>
-          </div>
+          <form className="login-inputs" onSubmit={handleSubmit(handleLogin)}>
+            <input
+              placeholder="Phone Number"
+              {...register("phoneNumber", {
+                required: true,
+                minLength: 10,
+                maxLength: 10,
+              })}
+            />
+            {errors.phoneNumber?.type === "required" && (
+              <p>phone number is required</p>
+            )}
+            <input
+              placeholder="password"
+              {...register("password", { required: true, minLength: 8 })}
+            />
+            {errors.password?.type === "required" && (
+              <p>password is required</p>
+            )}
+            <button className="login-btn" type="submit">
+              Login
+            </button>
+          </form>
         </p>
       </div>
     </div>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,16 +1,44 @@
 import React from "react";
 import "../styles/Signup.css";
+import { useForm } from "react-hook-form";
 
 function Signup() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const handlesignup = () => {
+    console.log("Handled Signup");
+  };
+
   return (
     <div className="signup-page">
       <p className="signup-heading">Signup</p>
-      <div className="signup-inputs">
-        <input type="text" placeholder="Username..." />
-        <input type="email" placeholder="Email..." />
-        <input type="password" placeholder="Password..." />
-        <button className="signup-btn">Signup</button>
-      </div>
+      <form onSubmit={handleSubmit(handlesignup)} className="signup-inputs">
+        <input
+          type="text"
+          placeholder="Username..."
+          {...register("username", { required: true })}
+        />
+        {errors.username?.type === "required" && <p>Username is required</p>}
+        <input
+          type="email"
+          placeholder="Email..."
+          {...register("email", { required: true })}
+        />
+        {errors.email?.type === "required" && <p>Email is required</p>}
+        <input
+          type="password"
+          placeholder="Password..."
+          {...register("password", { required: true })}
+        />
+        {errors.password?.type === "required" && <p>Password is required</p>}
+        <button className="signup-btn" type="submit">
+          Signup
+        </button>
+      </form>
     </div>
   );
 }
